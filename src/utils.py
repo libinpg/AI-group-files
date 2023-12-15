4e859239f0187d7578fc9fb2e637d17c961ab897
libinpg <101452633+libinpg@users.noreply.github.com>
2023-12-15 15:10:02 +0800

    Update utils.py
    添加百度千帆access_token获取函数

--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,16 @@ def load_config(config_file='config\config.json'):
     # except Exception as e:
     #     print(f"Error loading configuration: {e}")
     #     return None, None, None
+
+def get_baiduqianfan_access_token():
+    """
+    Generate the authentication signature (Access Token) using API_KEY and SECRET_KEY.
+    """
+    url = "https://aip.baidubce.com/oauth/2.0/token"
+    openai_api_key, baidu_api_key, baidu_api_secret = load_config()
+    if not baidu_api_key or not baidu_api_secret:
+        print("API key or Secret key not found. Please check the configuration file.")
+        return None
+    params = {"grant_type": "client_credentials", "client_id": baidu_api_key, "client_secret": baidu_api_secret}
+    response = requests.post(url, params=params).json()
+    return response.get("access_token")
