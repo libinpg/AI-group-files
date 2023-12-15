0e40a9846fb57bdde0570c1db6b5f991ad44f302
libinpg <101452633+libinpg@users.noreply.github.com>
2023-12-15 21:19:06 +0800

    Add files via upload
    添加单独定义prompts的文件

---
+++ src/prompts.py
@@ -0,0 +1,7 @@
+# prompts.py
+CLASSIFICATION_PROMPT = (
+    "You are a file organizing assistant. Based on this content, suggest a concise, valid folder name. "
+    "this is content: '{}'. Please format the response like {{folder_name}}, which means that your response "
+    "should be enclosed within single braces."
+)
+
