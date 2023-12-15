ed96e870c51ddc914dfa6b3043c10b22520613c3
libinpg <101452633+libinpg@users.noreply.github.com>
2023-12-15 14:52:48 +0800

    Update main.py
    添加了Llama-2-7b-chat的参数调用

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import argparse
 from file_parser import read_file_content
 from openai_classifier import classify_content as openai_classify
 from ChatGLM2_6B_32K_classifier import classify_content as glm_classify
+from Llama_2_7b_chat_classifier import classify_content as llama_classify
 from organizer import move_file_to_category_folder
 import os
 
@@ -19,7 +20,15 @@ def process_directory(directory, classifier_func):
             process_file(file_path, classifier_func)
 
 def main(target, classifier):
-    classifier_func = openai_classify if classifier == 'openai' else glm_classify
+    if classifier == 'openai':
+        classifier_func = openai_classify
+    elif classifier == 'glm':
+        classifier_func = glm_classify
+    elif classifier == 'llama':
+        classifier_func = llama_classify
+    else:
+        print(f"未知的分类器: {classifier}")
+        return
     if os.path.isdir(target):
         process_directory(target, classifier_func)
     elif os.path.isfile(target):
@@ -30,8 +39,9 @@ def main(target, classifier):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="自动文件整理系统")
     parser.add_argument("target_path", type=str, help="要整理的文件或目录的路径")
-    parser.add_argument("--classifier", type=str, choices=['openai', 'glm'], default='openai', 
-                        help="选择使用的分类器：'openai' 或 'glm'")
+    parser.add_argument("--classifier", type=str, choices=['openai', 'glm', 'llama'], default='openai',
+                        help="选择使用的分类器：'openai', 'glm' 或 'llama'")
     
     args = parser.parse_args()
     main(args.target_path, args.classifier)
+
